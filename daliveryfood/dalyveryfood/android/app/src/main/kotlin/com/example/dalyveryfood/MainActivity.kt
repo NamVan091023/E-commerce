16552a374608a16d4fd6fb0d98e37adaad16e1b1
NAMVAN\lnam4 <lnam40161@gmail.com>
2024-09-12 15:21:45 +0700

    Initial commit

---
+++ daliveryfood/dalyveryfood/android/app/src/main/kotlin/com/example/dalyveryfood/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dalyveryfood
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ daliveryfood/dalyveryfood/lib/Hompagefood/foodhomescreen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class _HomeFoodmainState extends StatefulWidget {
+  const _HomeFoodmainState({super.key});
+
+  @override
+  State<_HomeFoodmainState> createState() => __HomeFoodmainStateState();
+}
+
+class __HomeFoodmainStateState extends State<_HomeFoodmainState> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
